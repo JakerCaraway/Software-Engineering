bfdfb1e3b0ef334c9ca9d8cb8e276b8250013767
Orrible-Amroth <o.benton@rocketmail.com>
2016-04-20 10:53:35 +0100

    Added files via upload

---
+++ Player.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package run.microbial;
+
+/**
+ *
+ * @author oab24
+ */
+public class Player {
+    private AntBrain antBrainName;
+    private int score;
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public void setScore(int newScore){
+        score = newScore;
+    }
+    
+    public void setAntBrain(AntBrain newAntBrain){
+        antBrainName = newAntBrain;
+    }
+    
+    public AntBrain getAntBrain(){
+        return antBrainName;
+    }
+}
