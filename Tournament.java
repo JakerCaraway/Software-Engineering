f36f33eb1d99ce36c837b045ad9f252049ee4b60
qwertcc <caowanqi@yahoo.com>
2016-04-27 14:27:26 +0100

    Update Tournament.java

--- Tournament.java
+++ Tournament.java
@@ -100,15 +100,42 @@ public class Tournament {
   }
   
 
-  
-  public void getRanking()
+    public static getRanking()
   {
+      scores = new HashMap<String, Integer>();
+      for (int i = 0; i < players.size(); i++ )
+      {
+         Player x = players.get(i);
+         scores.put(x.getPlayerName(), getScores(x));
+      }
       
+      Map<String, Integer> playerByOrder = sortPlayer(scores);
   }
+ 
+ private static Map<String, Integer> sortPlayer(Map<String, Integer> unsortedPlayer)
+  {
+      List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortedPlayer.entrySet());
+      Collections.sort(playerByOrder, new Comparator<score>())
+      {
+        public int compare(Entry<String, Integer> e1, Entry<String,Integer> e2)
+        {
+          return e2.getValue.compareTo(e1.getValue())
+        }
+      }
+      Map<String, Integer> playerByOrder = new LinkedHashMap<String, Integer>();
+      
+      for (scores entry : list)
+      {
+       playerByOrder.put(entry.getKey(), entry.getValue());
+       
+      }
+      return playerByOrder;
+  }
+      
   
   public void displayRanking()
   {
-    
+     printMap(playerByOrder);
   }
   
   
