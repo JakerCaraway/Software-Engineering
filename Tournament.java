61ca4ac9f7e3b15cc5915e132d6cf3937604792f
qwertcc <caowanqi@yahoo.com>
2016-05-06 13:23:19 +0100

    Update Tournament.java

--- Tournament.java
+++ Tournament.java
@@ -25,28 +25,33 @@ public class Tournament {
  
  
   private Player player;
-  private HashMap<String, Integer> playerScores;//Store player's name and scores
+  private HashMap<String, Integer> playerScores;
   private ArrayList<Player> players;
   private World world;
 
-  
-
-
-//Create a tournament
-  public Tournament(ArrayList<Player> player, World world)
-  {  
+  /**
+   * This constructor of Tournament is used to add a new player and a new ant world for tournament. 
+   * @param player a new player
+   * @param world a new ant world
+   */
+  public Tournament(ArrayList<Player> player, World world){  
        players = player;
        this.world = world;
-  }
+   }
   
-  // gets the players list
+    /**
+     * gets the ArrayList of the players
+     * @return An ArrayList of the players
+     */
     public ArrayList<Player> getPlayers() {
         return players;
     }
 
- // Run a tounament
-  public void runTournament()
-  {
+    /**
+     * Run a tournament between players.
+     */
+    public void runTournament(){
+        
       for(Player a : players)
           for(Player b : players)
           {
@@ -54,7 +59,7 @@ public class Tournament {
              {
                  System.out.println("Match between " + a.getName()+ " and " + b.getName() +" now starting");
                  Game game1 = new Game(world,a,b);
-                 Game game2 = new Game(world,b,a); //Create a second match with players playing different colour
+                 Game game2 = new Game(world,b,a); 
                  game1.run();
                  System.out.println("Match between " + a.getName()+ " and " + b.getName() +" now switching places");
                  game2.run();
@@ -62,18 +67,22 @@ public class Tournament {
              }
           }
       checkWinner();
-                 
-  }
-//Get player's scores
-  public int getScore(Player x)
-  {
-    return x.getScore();
-  }
-  
- 
- //Create a comparator to compare players' scores
- private static Map<String, Integer> sortPlayers(Map<String, Integer> unsortedPlayer)
-  {
+    }
+    
+    /**
+     * gets the score of the player from Player class
+     * @return the int score of the player
+     */
+     public int getScore(Player x){
+      
+      return x.getScore();
+    }
+     
+     /**
+     * Create a comparator to compare scores of the players
+     * @return The HashMap of players name with scores in descending order
+     */
+    private static Map<String, Integer> sortPlayers(Map<String, Integer> unsortedPlayer){
       List<Entry<String, Integer>> list = new LinkedList<>(unsortedPlayer.entrySet());
       Collections.sort(list, new Comparator<Entry<String, Integer>>() {
 
@@ -92,41 +101,43 @@ public class Tournament {
        
       }
       return playerByOrder;
-  }
+     }
       
- //Check if there is only one winner
-  public void checkWinner()
-  {
-    playerScores = new HashMap<>();
-    for (Player x : players) 
-    {
-      playerScores.put(x.getName(), x.getScore());
-    }
+    /**
+     * Check if there is only one player with highest score.
+     */
+    public void checkWinner(){
       
-    Map<String, Integer> playerByOrder = sortPlayers(playerScores);
-
-    int h = 0;//the number of player with highest scores
-    String name = "";
-    int highestScore = (Collections.max(playerScores.values())); 
-    for (Entry<String, Integer> entry : playerScores.entrySet())
-     {      
-       if (entry.getValue() == highestScore)
+      playerScores = new HashMap<>();
+        for (Player x : players) 
          {
-            h++;
-            name = entry.getKey();
-         } 
-      }
-   if (h != 1)
-    {
-      runTournament(); // No clear Winner, Run a new tournament
-    }
-   if (h == 1) // there is only one player with highest score
-    {
-        System.out.println("Winner: " + name + " !");
-        for (Entry<String, Integer> entry: playerByOrder.entrySet())
-        {
+           playerScores.put(x.getName(), x.getScore());
+         }
+      
+      Map<String, Integer> playerByOrder = sortPlayers(playerScores);
+
+      int h = 0;
+      String name = "";
+      int highestScore = (Collections.max(playerScores.values())); 
+      for (Entry<String, Integer> entry : playerScores.entrySet())
+       {      
+         if (entry.getValue() == highestScore)
+           {
+              h++;
+              name = entry.getKey();
+           } 
+       }
+      if (h != 1)
+       {
+         runTournament(); 
+        }
+      if (h == 1) 
+       {
+          System.out.println("Winner: " + name + " !");
+          for (Entry<String, Integer> entry: playerByOrder.entrySet())
+          {
             System.out.println(playerByOrder);
+          }
         }
     }
-  }
 }
